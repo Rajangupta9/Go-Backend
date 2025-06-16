ecf970655b9ca949b43e1e61983aecfac7982439
Rajan Gupta <rajang797@gmail.com>
2025-06-16 12:07:46 +0530

    inital

---
+++ handlers/task.go
@@ -0,0 +1,78 @@
+package handlers
+
+import (
+	"GoBackend/config"
+	"GoBackend/models"
+	"GoBackend/utils"
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+	userId := r.Context().Value("user_id").(string)
+
+	var task models.Task
+
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		utils.ResponseWithError(w, http.StatusBadRequest, "Invalid request")
+		return
+	}
+
+	task.UserID = userId
+	task.CreatedAt = time.Now()
+	task.UpdatedAt = time.Now()
+
+	collection := config.MongoClient.Database("task_db").Collection("tasks")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if t, err := collection.InsertOne(ctx, task); err != nil {
+		if mongo.IsDuplicateKeyError(err) {
+			utils.ResponseWithError(w, http.StatusConflict, "task already exists")
+		} else {
+			utils.ResponseWithError(w, http.StatusInternalServerError, "failed to create task")
+		}
+		return
+	} else {
+		task.ID = t.InsertedID.(primitive.ObjectID)
+
+		utils.ResponseWithJson(w, http.StatusCreated, task)
+	}
+
+}
+
+func ListAllTask(w http.ResponseWriter, r *http.Request) {
+	userIdStr := r.Context().Value("user_id").(string)
+
+
+	
+
+	collection := config.MongoClient.Database("task_db").Collection("tasks")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+
+	cursor, err := collection.Find(ctx, bson.M{"user_id": userIdStr})
+	if err != nil {
+		utils.ResponseWithError(w, http.StatusInternalServerError, "Failed to fetch tasks")
+		return
+	}
+	defer cursor.Close(ctx)
+
+	var allTasks []models.Task
+	if err = cursor.All(ctx, &allTasks); err != nil {
+		utils.ResponseWithError(w, http.StatusInternalServerError, "Failed to decode tasks")
+		return
+	}
+
+	utils.ResponseWithJson(w, http.StatusOK, allTasks)
+}
+
